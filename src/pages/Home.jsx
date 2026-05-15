268931c2432527012c853a2f79dbdb3abdebbb21
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-05-15 17:18:39 +0600

    assignment done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,9 @@ const Home = () => {
         <Header></Header>
       </section>
       <div className="max-w-7xl mx-auto px-4 py-12">
-        <h2 className="text-4xl font-bold text-center mb-10">Our Best Chefs</h2>
+        <h2 className="text-4xl font-bold text-center mb-10">
+          Our Best Chefs in the Town
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {chefs.map((chef) => (
             <ChefCard chef={chef} key={chef.id}></ChefCard>
